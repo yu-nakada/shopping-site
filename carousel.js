eb6355731513c516f9ed829095e51f6edec881a3
EC2 Default User <ec2-user@ip-172-31-21-31.us-east-2.compute.internal>
2018-05-27 12:22:11 +0000

    課題再提出_1

--- carousel.js
+++ carousel.js
@@ -17,8 +17,8 @@ $(document).ready(function(){
 });
 
 $(window).load(function(){
-  $("#list_img img").click(function(){
+  $(".list_img img").click(function(){
     var img_src = $(this).attr("src");
-    $("#display_img img").attr("src", img_src);
+    $(".display_img img").attr("src", img_src);
   });
 });
